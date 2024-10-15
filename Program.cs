ef83ece2c96ee01d2b7d80fb15c30d71b9711e6c
Lucy-612 <233598@students.au.edu.pk>
2024-10-15 14:19:17 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _233598_Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 4, 5, 6, 7, 6, 7, 2 };
+            foreach(int i in number)
+            {
+            System.Console.WriteLine("{0}", i);
+            }
+
+
+        }
+    }
+}
